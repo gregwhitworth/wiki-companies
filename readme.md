fbc1c391c7a6cc1e7f67b876568a24a0c063864e
Greg Whitworth <gwhit@microsoft.com>
2019-03-22 20:40:07 -0700

    Added a way to scrape numerous versions

--- readme.md
+++ readme.md
@@ -9,4 +9,8 @@ You need both `requests` and `lxml` installed via pip on Python 3.
 If you add a new list, please create it similiar to `companies_list.py` but 
 name it specific to what the list represents, eg: (sp500_list.py).
 
+* Add a new URL to the list of `urls`
+* Add the name of the file to the `fileName` list
+* Add the xpath string to `urlXpath` list
+
 Submit a PR.
--- scrape.py
+++ scrape.py
@@ -2,14 +2,29 @@ from lxml import html
 import requests
 import io
 
+urls = [
+    "https://en.m.wikipedia.org/wiki/List_of_S%26P_500_companies"
+]
+
+fileName = [
+    "sp500"
+]
+
+urlXpath = [
+    '//table[@id="constituents"]/tbody/tr/td[position()=1]/a/text()'
+]
+
 def getCompanies():
+    companies = []
     print("Getting companies")
-    page = requests.get('https://en.m.wikipedia.org/wiki/List_of_S%26P_500_companies')
-    tree = html.fromstring(page.content)
-    companies = tree.xpath('//table[@id="constituents"]/tbody/tr/td[position()=1]/a/text()')
+    for i in range(len(urls)):
+        page = requests.get(urls[i])
+        tree = html.fromstring(page.content)
+        companies += tree.xpath(urlXpath[i])
+        compileOutput(companies, fileName[i])
     return companies
 
-def compileOutput(companies):
+def compileOutput(companies, fileName):
     print("Compiling output")
     output = 'companies = ['
 
@@ -23,18 +38,18 @@ def compileOutput(companies):
             output += '"'
     output += "\n]"
     
-    return output
+    writeCompaniesToFile(output, fileName)
 
-def writeCompaniesToFile(output):
+def writeCompaniesToFile(output, fileName):
     print("Writing companies to file")
-    file = open('companies_list.py','w')
+    tmpFileName = fileName + '_list.py'
+    file = open(tmpFileName, 'w')
     file.write(output)
     return file.close()
 
 def main():
     companies = getCompanies()
-    output = compileOutput(companies)
-    writeCompaniesToFile(output)
+    output = compileOutput(companies, 'companies')
 
 if __name__ == '__main__':
     main()
